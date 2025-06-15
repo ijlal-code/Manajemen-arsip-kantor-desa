37cb6cf0c8521ece35411c25086b4a2c9f49d26b
Ainun Ijlal <ijlalcode@gmail.com>
2025-06-15 21:09:42 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ Route::get('/', function () {
 
 
 
-
-
-
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
